1fdd9b24cd6d72096ae605825b361db72b55b3d9
ValenzuelaH <horacio.valenzuela@alu.unq.edu.ar>
2019-11-07 10:56:27 -0300

    Fixing add All books and Adding data annotation in Book

--- src/main/kotlin/grupoSinNombre/model/Book.kt
+++ src/main/kotlin/grupoSinNombre/model/Book.kt
@@ -5,7 +5,7 @@ import javax.persistence.GeneratedValue
 import javax.persistence.Id
 
 @Entity
-class Book(
+data class Book(
         var name: String,
         var authorName: String,
         var releaseYear: Int,
