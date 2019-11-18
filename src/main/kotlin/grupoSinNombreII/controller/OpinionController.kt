19a0988cdfc3ecb1403c7c0fc6ab4888abbbd30a
ValenzuelaH <horacio.valenzuela@alu.unq.edu.ar>
2019-11-18 18:15:12 -0300

    Fixing opinion

--- src/main/kotlin/grupoSinNombreII/controller/OpinionController.kt
+++ src/main/kotlin/grupoSinNombreII/controller/OpinionController.kt
@@ -17,18 +17,18 @@ class OpinionController(var opinionService : OpinionService) {
     @PostMapping("/new_opinion")
     fun create(@RequestBody opinion : Opinion) = opinionService.create(opinion)
 
-    @RequestMapping("/{id}")
-    fun findById(id : Long) = opinionService.findById(id)
+    @RequestMapping("/id/{id}")
+    fun findById(@PathVariable id : Long) = opinionService.findById(id)
 
     @RequestMapping("/all_opinion")
     fun findAll() = opinionService.findAll()
 
     @RequestMapping("/op_book/{id}")
-    fun findByBook(id : Long) = opinionService.findByBook(id)
+    fun findByBook(@PathVariable id : Long) : MutableList<Opinion> = opinionService.findByBook(id)
 
     @RequestMapping("/op_user/{id}")
-    fun findByUser(id : Long) = opinionService.findByUser(id)
+    fun findByUser(@PathVariable id : Long) = opinionService.findByUser(id)
 
     @RequestMapping("/find_especific/{id_book}/{id_user}")
-    fun findEspecific(id_book : Long, id_user : Long) = opinionService.findEspecific(id_book, id_user)
+    fun findEspecific(@PathVariable id_book : Long, @PathVariable id_user : Long) = opinionService.findEspecific(id_book, id_user)
 }
--- src/main/kotlin/grupoSinNombreII/model/Opinion.kt
+++ src/main/kotlin/grupoSinNombreII/model/Opinion.kt
@@ -4,15 +4,13 @@ import java.time.LocalDateTime
 import javax.persistence.*
 
 @Entity
-@Table(name = "opinion")
 data class Opinion(@JoinColumn(name = "id_user", nullable = false)
-                   @ManyToOne(optional = false, cascade = [CascadeType.ALL], fetch = FetchType.EAGER)
+                   @ManyToOne(optional = false, cascade = [], fetch = FetchType.EAGER)
                    val user : User,
                    @JoinColumn(name = "id_book", nullable = false)
-                   @ManyToOne(optional = false, cascade = [CascadeType.ALL], fetch = FetchType.EAGER)
+                   @ManyToOne(optional = false, cascade = [], fetch = FetchType.EAGER)
                    val book : Book,
                    var date : LocalDateTime = LocalDateTime.now(),
                    var msj: String,
-                   @Id
-                   @GeneratedValue(strategy = GenerationType.AUTO)
+                   @Id @GeneratedValue
                    val id : Long = 0)
