fe8f46c839462340a0b42bbcf91e4bef4894174a
ValenzuelaH <horacio.valenzuela@alu.unq.edu.ar>
2019-11-30 20:34:22 -0300

    Fixing Cors

--- src/main/kotlin/grupoSinNombreII/controller/MailController.kt
+++ src/main/kotlin/grupoSinNombreII/controller/MailController.kt
@@ -4,16 +4,20 @@ import grupoSinNombreII.model.MailWrapper
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
+
+@CrossOrigin(origins= ["http://localhost:3000"])
 @RestController
 class MailController {
 
        @Autowired
        private lateinit var javaMailSender: JavaMailSender
 
+    @CrossOrigin
     @PostMapping("/mail")
     fun enviarMail(@RequestBody mailWrapper: MailWrapper) = this.sendEmail(mailWrapper);
 
